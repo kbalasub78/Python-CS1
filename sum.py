d8de82d263a7052828b8dcfaf27663d173131456
Balasubramanian Kalyanaraman <kbalasub78@gmail.com>
2017-08-11 17:06:49 +1000

    Updated sum.py file with comments

--- sum.py
+++ sum.py
@@ -1,7 +1,12 @@
+# This program gets two numbers from user, sums them up and displays the sum value
+# Get first number from User
 num1 = int( input('Enter first number: ') )
 print(num1)
+
+# Get second number from User
 num2 = int( input('Enter second number: ') )
 print(num2)
 
+# Calculate sum of the two numbers and print the sum
 sum = num1 + num2
 print('Sum value is ' + str(sum) )
