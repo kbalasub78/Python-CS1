964b62a96b43241009e776acc43433bd2193b0c8
Balasubramanian Kalyanaraman <kbalasub78@gmail.com>
2017-08-16 11:35:47 +1000

    Update chapter03-exercise01.py

--- py4e-chapter03/chapter03-exercise01.py
+++ py4e-chapter03/chapter03-exercise01.py
@@ -3,7 +3,7 @@ print('Chapter 3 - Excerise 1')
 hours = float( input('Enter Hours: ') )
 rate = float( input('Enter Rate: ') )
 
-if hours < 40 :
+if hours <= 40 :
     pay = hours * rate
 else :
     pay = (40 * rate) + ( (hours - 40) * (rate * 1.5) )
