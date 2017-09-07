92b6ad8da2fb6085c750e01a032a868ff44a620f
Balasubramanian Kalyanaraman <kbalasub78@gmail.com>
2017-09-07 14:58:28 +1000

    Update alarm_clock.py

--- checkpoint2/alarm_clock.py
+++ checkpoint2/alarm_clock.py
@@ -6,50 +6,39 @@
 ## Unless we are on vacation
 ## -- then on weekdays it should be "10:00" and weekends it should be "off".
 ##
+
 ## import sys module for graceful data input handling
 import sys
 
-def alarm_clock(dayValue, vacationFlag):
-    ## Find alarm time by validating the condition
-    if(dayValue == 0 or dayValue == 6):
-        weekendFlag = True
-    else:
-        weekendFlag = False
-    
-    if vacationFlag == 'false' : ## Not on vacation
-        if not weekendFlag :
-            alarmValue = "7:00"
+## alarm_clock module definition
+def alarm_clock(day, onVacation):    
+    if not onVacation : ## Not on vacation
+        if day in range(1,6) :
+            alarm = "7:00"
         else:
-            alarmValue = "10:00"
-    elif vacationFlag == 'true' : ## On vacation
-        if not weekendFlag:
-            alarmValue = "10:00"
+            alarm = "10:00"
+    elif onVacation : ## On vacation
+        if day in range(1,6):
+            alarm = "10:00"
         else:
-            alarmValue = "Off"
+            alarm = "Off"
     
-    return alarmValue
+    return alarm
 
 ## Get input of speed and birthday
 while True:
     try:
         dayValue = int(input("Enter the day of the week (0=Sun, 1=Mon, 2=Tue,..,6=Sat) : "))
-        vacationFlag = input("Are you on vacation (True or False): ").lower()
-        if (vacationFlag != 'true' and vacationFlag != 'false') :
+        vacation = input("Are you on vacation (True or False): ").title()
+        if (vacation != 'True' and vacation != 'False') :
             raise ValueError("Input data format is incorrect")
+
+        if(vacation == 'True'): vacationFlag = True
+        elif(vacation == 'False'): vacationFlag = False
+
         break
     except:
         print("Error in data entered", sys.exc_info())
 
 ## Print the alarm setting
 print( "Alarm setting : ", alarm_clock( dayValue, vacationFlag) )
-
-## alarm_clock function validation for different inputs
-##print( alarm_clock( 1, 'false')) ## 7:00
-##print( alarm_clock( 5, 'false')) ## 7:00
-##print( alarm_clock( 0, 'false')) ## 10:00
-##print( alarm_clock( 6, 'false')) ## 10:00
-##print( alarm_clock( 0, 'true')) ## Off
-##print( alarm_clock( 6, 'true')) ## Off
-##print( alarm_clock( 1, 'true')) ## 10:00
-##print( alarm_clock( 3, 'true')) ## 10:00
-##print( alarm_clock( 5, 'true')) ## 10:00
