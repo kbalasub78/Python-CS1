d098fabb044c433779e9359b79008fc04c09ba1a
Balasubramanian Kalyanaraman <kbalasub78@gmail.com>
2017-09-14 16:55:18 +1000

    Create exercise02.py

---
+++ py4e-chapter06/exercise02.py
@@ -0,0 +1,10 @@
+## Exercise 2: Given that fruit is a string, what does fruit[:] mean?
+
+fruit = 'pineapple'
+
+## print fruit in usual way
+print(fruit)
+
+## Below is same as printing fruit string
+## indicating to start from character at index 0 and go till last character
+print( fruit[:] )
